571d7692f9f738ab0d44c86ef5c67954ec5b1898
Snehal Yelkar <snehal.yelkar@centricsoftware.com>
2025-07-30 12:08:46 +0530

    update import

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,13 @@ import { ToolkitTenantProvider } from "@centricsoftware/toolkit-tenant";
 import { ChatPage } from "@centricsoftware/toolkit-tenant";
 
 // Minimal mock store for testing
-const mockStore = {
-  getData: () => "Consumer app test data"
-};
+// const mockStore = {
+//   getData: () => "Consumer app test data"
+// };
 
 export default function App() {
   return (
-    <ToolkitTenantProvider dataStore={mockStore}>
+    <ToolkitTenantProvider runtimeUrl="http://127.0.0.1:4000/api/chat">
       <ChatPage onSubmitMessage={() => {}} user={{ id: "consumer" }} tools={
         <>
           {/* Place your tools/components here */}
